16463a9d8738ce53962b47fce48dc5bb0ea812d5
Dominik <cogentdom@gmail.com>
2022-02-24 21:42:17 -0800

    ...

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ import seaborn as sns
 import requests
 import json
 
-plt.style.use('seaborn-darkgrid')
-plt.rc('patch', force_edgecolor=True,edgecolor='black')
-plt.rc('hist', bins='auto')
-sns.set_context('notebook')
-sns.set_palette('gist_heat')
+# plt.style.use('seaborn-darkgrid')
+# plt.rc('patch', force_edgecolor=True,edgecolor='black')
+# plt.rc('hist', bins='auto')
+# sns.set_context('notebook')
+# sns.set_palette('gist_heat')
+st.title("Stock Display App")
 st.set_page_config(layout='wide')
 
-url = 'https://api-stock-getter-rghwdwjikq-uc.a.run.app/price/aapl'
+url = 'https://api-stock-getter-rghwdwjikq-uc.a.run.app/price/'
 
-ticker_list = ["AAPL", "GOOG", "MSFT", "TSLA"]
+ticker_list = {"Apple":"AAPL", "Google":"GOOG", "Microsoft":"MSFT", "Tesla":"TSLA"}
+
+stocks = ("Apple", "Google", "Microsoft", "Tesla")
+selected_stock = st.selectbox("Select Dataset for prediction", stocks)
+url = url + ticker_list[selected_stock]
 
 response = requests.get(url)
 data = json.loads(response.text)
 df = pd.DataFrame(data)
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
-df = df.resample('2w').mean()
+df['Date'] = pd.to_datetime(df.Date, unit='ms')
+df.set_index('Date', inplace=True, drop=True)
+
 print(df)
 
 def decomp_plot(df):
